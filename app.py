21a2cc0b5ff72a0d4ab8ddf05b9e85dc38483a57
tanyatiwai660-stack <tanyatiwai660@gmail.com>
2026-01-31 05:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, request, jsonify
 from deep_translator import GoogleTranslator
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)   # ✅ ALLOW ALL ORIGINS (important)
+
 translator = GoogleTranslator(source="en", target="hi")
 
 @app.route("/translate", methods=["POST"])
@@ -14,6 +17,10 @@ def translate():
         return jsonify(result="")
 
     return jsonify(result=translator.translate(text))
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Backend is running"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
